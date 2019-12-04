cd0ac83001f2606991dff5ce854ba654e6c5498b
ninad32687 <ninadkadam32687@gmail.com>
2019-12-04 05:56:08 +0530

    initial app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ProductListComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
