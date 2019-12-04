7300d3393208fb6bf9be8c49e7d8df0a8e57cff2
ninad32687 <ninadkadam32687@gmail.com>
2019-12-04 14:31:57 +0530

    app updated

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductService {
     
-  url='http://192.168.41.226:9898/product'
+  url='http://172.18.4.79:9898/product'
   constructor(private http:HttpClient) { }
 
 
